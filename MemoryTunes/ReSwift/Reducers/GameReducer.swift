3b7b011ad5828e5a1636960b04842c00b38ef202
James Rochabrun <j@Jamess-MacBook-Pro-2.local>
2018-01-21 18:57:41 -0800

    final

--- MemoryTunes/ReSwift/Reducers/GameReducer.swift
+++ MemoryTunes/ReSwift/Reducers/GameReducer.swift
@@ -5,6 +5,23 @@
 import ReSwift
 
 func gameReducer(action: Action, state: GameState?) -> GameState {
-  let state = state ?? GameState(memoryCards: [], showLoading: false, gameFinished: false)
+  
+  var state = state ?? GameState(memoryCards: [], showLoading: false, gameFinished: false)
+  
+  switch action {
+    // 1 On FetchTunesAction, this sets showLoading to true.
+  case _ as FetchTunesAction:
+    state = GameState(memoryCards: [], showLoading: true, gameFinished: false)
+    // 2  On SetCardsAction, this randomizes the cards and sets showLoading to false. generateNewCards can be found in MemoryGameLogic.swift, which is included with the starter.
+  case let setCardsAction as SetCardsAction:
+    state.memoryCards = generateNewCards(with: setCardsAction.cardImageUrls)
+    state.showLoading = false
+    
+    //3 For a FlipCardAction, flipCard changes the state of the memory cards based on the cardIndexToFlip and other game logic. hasFinishedGame is called to determine if the game has finished and update the state accordingly. Both functions can be found in MemoryGameLogic.swift.
+  case let flipCardAction as FlipCardAction:
+    state.memoryCards = flipCard(index: flipCardAction.cardIndexToFlip, memoryCards: state.memoryCards)
+    state.gameFinished = hasFinishedGame(cards: state.memoryCards)
+  default: break
+  }
   return state
 }
--- MemoryTunes/ViewControllers/GameViewController.swift
+++ MemoryTunes/ViewControllers/GameViewController.swift
@@ -1,39 +1,46 @@
 /**
  * Copyright (c) 2017 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
  */
 
 import UIKit
+import ReSwift
 
 final class GameViewController: UIViewController {
   
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+  var collectionDataSource: CollectionDataSource<CardCollectionViewCell, MemoryCard>?
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    store.subscribe(self) {
+      $0.select {
+        $0.gameState
+      }
+    }
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    store.unsubscribe(self)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    //1 Dispatches fetchTunes to start fetching the images from iTunes API. (network call)
+    store.dispatch(fetchTunes)
+    collectionView.delegate = self
+    loadingIndicator.hidesWhenStopped = true
+    
+    //2 Configures cells using CollectionDataSource which gets the appropriate model to configureCell.
+    collectionDataSource = CollectionDataSource(cellIdentifier: "CardCell", models: [], configureCell: { (cell, model) -> CardCollectionViewCell in
+      cell.configureCell(with: model)
+      return cell
+    })
+    
+    collectionView.dataSource = collectionDataSource
+  }
+  
   
   fileprivate func showGameFinishedAlert() {
     let alertController = UIAlertController(title: "Congratulations!",
@@ -50,5 +57,40 @@ final class GameViewController: UIViewController {
 // MARK: - UICollectionViewDelegate
 extension GameViewController: UICollectionViewDelegate {
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    ///The final piece of the puzzle is to send a flip action when a card is selected. This will kick off the game logic and make associated state changes.
+    store.dispatch(FlipCardAction(cardIndexToFlip: indexPath.row))
   }
 }
+
+//MARK: - StoreSubscriber
+extension GameViewController: StoreSubscriber {
+  
+  /// This implements newState to handle state changes. It updates the datasource as well as:
+  
+  func newState(state: GameState) {
+    
+    collectionDataSource?.models = state.memoryCards
+    collectionView.reloadData()
+    
+    //1 Updating the loading indicator status depending on the state.
+    state.showLoading ? loadingIndicator.startAnimating() : loadingIndicator.stopAnimating()
+    
+    //2 Restarting the game and showing an alert when the game has been finished.
+    if state.gameFinished {
+      showGameFinishedAlert()
+      /// restart
+      store.dispatch(fetchTunes)
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
